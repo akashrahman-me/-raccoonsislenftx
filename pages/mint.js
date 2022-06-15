bc8db3abfcf4ef99577421d16fd02955346bc16d
akashrahman-me <akashrahman.me@gmail.com>
2022-06-16 01:03:53 +0600

    some

--- pages/mint.js
+++ pages/mint.js
@@ -5,9 +5,9 @@ export default function Mint() {
   return (
     <div className="relative">
       <Navbar defaultColor />
-      <div class="pt-[89px]">
-        <div class="flex h-[calc(100vh-90px)] items-center justify-center">
-          <div class="max-w-[500px]">
+      <div className="pt-[89px]">
+        <div className="flex h-[calc(100vh-90px)] items-center justify-center">
+          <div className="max-w-[500px]">
             <img src="/images/loading2.gif" class="w-full" />
           </div>
         </div>
